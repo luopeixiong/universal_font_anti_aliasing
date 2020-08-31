d6bb17c4d911a3368286d7c28501499baa593f4c
Your Name <you@example.com>
2020-08-31 11:54:53 +0800

    修复

--- universal_font_anti_aliasing/anti_aliasing.py
+++ universal_font_anti_aliasing/anti_aliasing.py
@@ -9,7 +9,7 @@ from .woff2tff import woff2tff
 base_dir = os.path.dirname(__file__)
 
 
-dll = CDLL(os.path.join(base_dir, "tyc_ocr.dll"))
+dll = CDLL(os.path.join(base_dir, "tyc_ocr_linx.so"))
 dll.NewOcr.argtype = c_char_p
 dll.NewOcr.restype = c_char_p
 
