c67257fb93dce104a982ad481dc426ec2dcbc931
Your Name <you@example.com>
2020-08-31 13:07:02 +0800

    修复支持linux

--- test_ocr.py
+++ test_ocr.py
@@ -1,9 +1,10 @@
 # coding:utf-8
 
-from universal_font_anti_aliasing.anti_aliasing import anti
+from universal_font_anti_aliasing.anti_aliasing import anti, load_dll
 
 
 if __name__ == '__main__':
+    load_dll()
     url = "https://s3plus.meituan.net/v1/mss_73a511b8f91f43d0bdae92584ea6330b/font/85226b1a.woff"
     s = "丙232"
     print("result is :", anti(url, s).decode("utf-8"))
